Evolution-XYZ-Devices/device_nokia_PL2

PL2: Decommonize device tree Signed-off-by: userariii <abhattacharjee717@gmail.com> Signed-off-by: Akshat <Legendleo90@users.noreply.github.com>

AndroidProducts.mk
@@ -1,0 +1,23 @@
+#
+# Copyright (C) Evolution-X
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/evolution_PL2.mk
+
+COMMON_LUNCH_CHOICES := \
+    evolution_PL2-eng \
+    evolution_PL2-user \
+    evolution_PL2-userdebug

crdroidandroid/android_device_nokia_PL2

AndroidProducts.mk
@@ -1,0 +1,23 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_PL2.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_PL2-eng \
+    lineage_PL2-user \
+    lineage_PL2-userdebug